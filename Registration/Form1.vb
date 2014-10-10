9a650339a447dd7ea9fc13ac8b460cf94759d358
Dark98 <darkaidz98@gmail.com>
2014-10-10 09:35:31 +0100

    It Works

--- Registration/Form1.vb
+++ Registration/Form1.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" And TextBox2.Text = "" Then
+            Form3.Show()
+            Me.Close()
+        Else
+            MsgBox("Invalid Details", 0, "Wrong Username or Password")
+        End If
+    End Sub
 End Class
